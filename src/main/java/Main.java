48635437e505ed4fee2e2b3671c2bbdd1e015271
general-suluguni <167622542+general-suluguni@users.noreply.github.com>
2024-05-07 17:50:26 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Введите расположение файла: ");
         String inp_file_name = scan.next();
         try {
             FileReader fileReader = new FileReader(inp_file_name);
