ef285d69e4ed4a30b1f52a6de75286ee5524561e
osina <osina1583@mail.ru>
2024-05-10 00:09:48 +0300

    added or fixed something, idk bro

--- src/test/java/Speed_Test.java
+++ src/test/java/Speed_Test.java
@@ -11,34 +11,34 @@ import java.util.Scanner;
 
 public class Speed_Test {
     public static void main(String[] args) {
-                ArrayList<Integer> numbers = new ArrayList<>();
-                Random random = new Random();
-                System.out.println("Введите необходимое количество чисел в файле: ");
-                Scanner scan = new Scanner(System.in);
-                int len = 0;
-                try {
-                    len = scan.nextInt();
-                }
-                catch (NumberFormatException ex) {
-                    System.out.println("Введено некорректное число: " + ex.getMessage());
-                }
-                    for (int i = 0; i < len; i++) {
-                        numbers.add(random.nextInt(-1000, 1000));
-                    }
-                    String testfile_name = "speed_test.txt";
-                    try (BufferedWriter writer = new BufferedWriter(new FileWriter(testfile_name))) {
-                        for (Integer num : numbers) {
-                            writer.write(num.toString());
-                            writer.write(" ");
-                        }
-                    } catch (IOException ex) {
-                        System.err.println("Ошибка при записи файла: " + ex.getMessage());
-                    }
-                    System.out.println("Тестовый файл успешно создан!");
-                    long t_start = System.currentTimeMillis();
-                    Main.func("speed_test.txt");
-                    long t_finish = System.currentTimeMillis();
-                    long time  = (t_finish - t_start);
-                    System.out.println("Время выполнения программы составило " + time + " миллисекунд.");
-            }
+        ArrayList<Integer> numbers = new ArrayList<>();
+        Random random = new Random();
+        System.out.println("Введите необходимое количество чисел в файле: ");
+        Scanner scan = new Scanner(System.in);
+        int len = 0;
+        try {
+            len = scan.nextInt();
         }
+        catch (NumberFormatException ex) {
+            System.out.println("Введено некорректное число: " + ex.getMessage());
+        }
+        for (int i = 0; i < len; i++) {
+            numbers.add(random.nextInt(-1000, 1000));
+        }
+        String testfile_name = "speed_test.txt";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(testfile_name))) {
+            for (Integer num : numbers) {
+                writer.write(num.toString());
+                writer.write(" ");
+            }
+        } catch (IOException ex) {
+            System.err.println("Ошибка при записи файла: " + ex.getMessage());
+        }
+        System.out.println("Тестовый файл успешно создан!");
+        long t_start = System.currentTimeMillis();
+        Main.func("speed_test.txt");
+        long t_finish = System.currentTimeMillis();
+        long time  = (t_finish - t_start);
+        System.out.println("Время выполнения программы составило " + time + " миллисекунд.");
+    }
+}
